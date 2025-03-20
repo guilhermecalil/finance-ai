2ba2fb664157067b87b19b2f1a900100410d814f
Guilherme Marangão <guilhermecalil5@gmail.com>
2025-03-19 22:45:03 -0300

    feat: add transactions pie chart component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,8 +2,10 @@ import { auth } from "@clerk/nextjs/server";
 import { isMatch } from "date-fns";
 import { redirect } from "next/navigation";
 import Navbar from "../_components/navbar";
+import { getDashboard } from "../_data/get-dashboard";
 import SummaryCards from "./_components/summary-cards";
 import TimeSelect from "./_components/time-select";
+import TransactionPieChart from "./_components/transaction-pie-chart";
 
 interface HomeProps {
   searchParams: {
@@ -23,6 +25,8 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
     redirect("?month=01");
   }
 
+  const dashboard = await getDashboard(month);
+
   return (
     <>
       <Navbar />
@@ -31,7 +35,14 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
           <h1 className="text-2xl font-bold">Dashboard</h1>
           <TimeSelect />
         </div>
-        <SummaryCards month={month} />
+        <div className="grid grid-cols-[2fr,1fr]">
+          <div className="flex flex-col gap-6">
+            <SummaryCards month={month} {...dashboard} />
+            <div className="grid grid-cols-3 grid-rows-1 gap-6">
+              <TransactionPieChart {...dashboard} />
+            </div>
+          </div>
+        </div>
       </div>
     </>
     // <div className="flex h-full items-center justify-center">
