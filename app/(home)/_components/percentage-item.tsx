55058bb3513807c46a41509bec4204b8e110895f
Guilherme Marangão <guilhermecalil5@gmail.com>
2025-03-19 23:31:54 -0300

    feat: ensure pages are correctly scrolled by the user

--- app/(home)/_components/percentage-item.tsx
+++ app/(home)/_components/percentage-item.tsx
@@ -10,7 +10,8 @@ const PercentageItem = ({ icon, title, value }: PercentageItemProps) => {
   return (
     <div className="flex items-center justify-between">
       {/* ícones */}
-      <div className="flex items-center gap-2">
+      <div className="flex items-center gap-3">
+        <div className="rounded-lg bg-white bg-opacity-[3%] p-2"></div>
         {icon}
         <p className="text-sm text-muted-foreground">{title}</p>
       </div>
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 import AddTransactionButton from "../_components/add-transaction-button";
 import Navbar from "../_components/navbar";
 import { DataTable } from "../_components/ui/data-table";
+import { ScrollArea } from "../_components/ui/scroll-area";
 import { db } from "../_lib/prisma";
 import { transactionColumns } from "./_columns";
 
@@ -22,13 +23,15 @@ const TransactionsPage = async () => {
   return (
     <>
       <Navbar />
-      <div className="space-y-6 p-6">
+      <div className="space-y-6 overflow-hidden p-6">
         {/* TÍTULO E BOTÃO */}
         <div className="flex w-full items-center justify-between">
           <h1 className="text-2xl font-bold">Transações</h1>
           <AddTransactionButton />
         </div>
-        <DataTable columns={transactionColumns} data={transactions} />
+        <ScrollArea>
+          <DataTable columns={transactionColumns} data={transactions} />
+        </ScrollArea>
       </div>
     </>
   );
