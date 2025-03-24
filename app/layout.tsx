a9e9dfb67889dbd9797202b08a4a54c474825881
Guilherme Marangão <guilhermecalil5@gmail.com>
2025-03-24 13:15:11 -0300

    feat: ensure transactions can be deleted

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import type { Metadata } from "next";
 import { Mulish } from "next/font/google";
+import { Toaster } from "sonner";
 import "./globals.css";
 
 const mulish = Mulish({
@@ -28,6 +29,8 @@ export default function RootLayout({
         >
           <div className="flex h-full flex-col overflow-hidden">{children}</div>
         </ClerkProvider>
+
+        <Toaster />
       </body>
     </html>
   );
--- app/transactions/_columns/index.tsx
+++ app/transactions/_columns/index.tsx
@@ -1,13 +1,12 @@
 "use client";
 
-import { Button } from "@/app/_components/ui/button";
 import {
   TRANSACTION_CATEGORY_LABELS,
   TRANSACTION_PAYMENT_METHOD_LABELS,
 } from "@/app/_constants/transactions";
 import { Transaction } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
-import { TrashIcon } from "lucide-react";
+import DeleteTransactionButton from "../_components/delete-transaction-button";
 import EditTransactionButton from "../_components/edit-transaction-button";
 import TransactionTypeBadge from "../_components/type-badge";
 
@@ -61,9 +60,7 @@ export const transactionColumns: ColumnDef<Transaction>[] = [
       return (
         <div className="space-x-1">
           <EditTransactionButton transaction={transaction} />
-          <Button variant="ghost" size="icon" className="text-muted-foreground">
-            <TrashIcon />
-          </Button>
+          <DeleteTransactionButton transactionId={transaction.id} />
         </div>
       );
     },
